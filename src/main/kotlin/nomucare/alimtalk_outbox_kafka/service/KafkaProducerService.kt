08b8bc8773a5ad505a3abaa24df10faa027ba17e
dev-chpark <xpmxf5@gmail.com>
2025-05-06 16:51:23 +0900

    fix : KafkaProducerService와 NotificationService에서 Kafka 토픽 이름을 'reservation'에서 'hotel-reservation-events'로 변경 - 이벤트의 성격을 명확히 하기 위해, 호텔 예약 도메인을 명확히 변경

--- src/main/kotlin/nomucare/alimtalk_outbox_kafka/service/KafkaProducerService.kt
+++ src/main/kotlin/nomucare/alimtalk_outbox_kafka/service/KafkaProducerService.kt
@@ -8,10 +8,13 @@ class KafkaProducerService(
     private val kafkaTemplate: KafkaTemplate<String, String>,
 ) {
     fun sendNotificationEvent(key: String, event: String) {
-        kafkaTemplate.send("reservation", key, event)
+        kafkaTemplate.send("hotel-reservation-events", key, event)
             .whenComplete { result, ex ->
             if (ex == null) {
                 println("메세지 전송 성공: $key")
+                println("메세지 전송 성공: ${result.recordMetadata.topic()} " +
+                        "파티션: ${result.recordMetadata.partition()} " +
+                        "오프셋: ${result.recordMetadata.offset()}")
             } else {
                 println("메세지 전송 실패: $key, 에러: ${ex.message}")
             }
