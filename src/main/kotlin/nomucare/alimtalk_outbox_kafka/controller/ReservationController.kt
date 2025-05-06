4b83a8ee42a3d2109ca6c8b1770d731f1fc3f978
dev-chpark <xpmxf5@gmail.com>
2025-05-06 16:13:10 +0900

    feat : ReservationController 구현 및 예약 생성, 조회 API 추가

---
+++ src/main/kotlin/nomucare/alimtalk_outbox_kafka/controller/ReservationController.kt
@@ -0,0 +1,39 @@
+package nomucare.alimtalk_outbox_kafka.controller
+
+import nomucare.alimtalk_outbox_kafka.model.Reservation
+import nomucare.alimtalk_outbox_kafka.service.ReservationService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/reservations")
+class ReservationController (
+    private val reservationService: ReservationService
+){
+    @PostMapping
+    fun createReservation(@RequestBody reservation: Reservation): ResponseEntity<Reservation> {
+        val createdReservation = reservationService.createReservation(reservation)
+        return ResponseEntity.ok(createdReservation)
+    }
+
+    @GetMapping("/{id}")
+    fun getReservationById(@PathVariable("id") id: Long): ResponseEntity<Reservation> {
+        val reservation = reservationService.getReservationById(id)
+        return if (reservation != null) {
+            ResponseEntity.ok(reservation)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @GetMapping
+    fun getAllReservations(): ResponseEntity<List<Reservation>> {
+        val reservations = reservationService.getAllReservations()
+        return ResponseEntity.ok(reservations)
+    }
+}
